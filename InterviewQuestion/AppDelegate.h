c1119b8fe2316d46cf9a570beee7c7b7302739a2
castial <897257524@qq.com>
2016-05-24 14:14:59 +0800

    first commit

---
+++ InterviewQuestion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InterviewQuestion
+//
+//  Created by 李洋 on 16/5/24.
+//  Copyright © 2016年 hyyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InterviewQuestion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InterviewQuestion
+//
+//  Created by 李洋 on 16/5/24.
+//  Copyright © 2016年 hyyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
